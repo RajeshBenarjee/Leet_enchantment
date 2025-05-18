f3cb5c435c03449d25e6e3fdbee968f82ad10732
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-05-18 13:57:49 +0530

    Time: 55 ms (10.52%) | Memory: 19 MB (29.62%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # array nums sorted in non-decreasing order --> Ascending order --> Atleast traverse once -- O(N)
+        # remove the duplicates 'in-place ' unique element appears only once -- poping -- O(1)
+        # relative order -- > maintain order --> can't sort 
+        # return the number of unique elements --> length of undate array -- inbuilt fun -- O(1)
+
+        # 1. traverse through the list using a pointer not for loop bcz of pop fun()
+        # 2. create a hash if ele already in hash pop it 
+        # 3. else add it to hash 
+        # 4. return the len of nums
+
+        l=0
+        hashy=set()
+        while l<len(nums):
+            if nums[l] in hashy:
+                nums.pop(l)
+            else:
+                hashy.add(nums[l])
+                l+=1
+        return len(nums)
