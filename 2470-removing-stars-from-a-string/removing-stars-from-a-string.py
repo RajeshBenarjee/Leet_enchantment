fec7856f8a99049bf7d0e2b24a9866c188c1876b
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-05-23 15:41:12 +0530

    Time: 153 ms (23.53%) | Memory: 18.9 MB (88.70%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -1,6 +1,8 @@
 class Solution:
     def removeStars(self, s: str) -> str:
         listy=[]
+        # n=len(s)
+        # T(N)=O(N) S(N)=O(N)
         for i in s:
             if listy==[]:
                 listy.append(i)
