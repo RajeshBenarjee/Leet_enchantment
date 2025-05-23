2a504bb9e9e613ef43d6dcf2bcb06e84e82ca107
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-05-23 10:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.91%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -15,21 +15,3 @@ class Solution:
 
 
 
-
-# class Solution:
-#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         if root==None:
-#             return []
-#         q=deque([root])
-#         res=[]
-#         while q:
-#             level=[]
-#             for i in range(len(q)):
-#                 node=q.popleft()
-#                 level.append(node.val)
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-#             res.append(level)
-#         return res
