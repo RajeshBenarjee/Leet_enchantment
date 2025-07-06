1b211808510f41df5219df6dbfb461be9394c2f7
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-07-07 05:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.74%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -2,16 +2,31 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        **This algo has three main steps**
+
+        - First find the nearest drop index (from the right). If no drop, reverse nums.
+        - If drop is there, find the smallest element greater than nums[drop] after it and swap.
+        - Reverse the remaining part after drop to get the next permutation.
         """
-        ind=-1
-        for i in range(len(nums)-2,-1,-1):
-            if(nums[i]<nums[i+1]):
-                ind=i
+        n = len(nums)
+        drop = -1
+        
+        # Step 1: Find the drop index (first decreasing element from right)
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                drop = i
                 break
-        if ind==-1:
-            return nums.reverse()
-        for i in range(len(nums)-1,-1,-1):
-            if(nums[ind]<nums[i]):
-                (nums[i],nums[ind])=(nums[ind],nums[i])
+        
+        if drop == -1:
+            nums.reverse()
+            return
+        
+        # Step 2: Find the smallest element greater than nums[drop] to the right
+        for i in range(n - 1, drop, -1):
+            if nums[i] > nums[drop]:
+                nums[i], nums[drop] = nums[drop], nums[i]
                 break
-        nums[ind+1:]=reversed(nums[ind+1:])
+        
+        # Step 3: Reverse the subarray after drop
+        nums[drop + 1:] = reversed(nums[drop + 1:])
