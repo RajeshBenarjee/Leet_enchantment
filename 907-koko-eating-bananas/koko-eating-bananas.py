7f5b478128ef193a04c78257e6726af4ac1c2ad7
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-07-21 01:53:42 +0530

    Time: 153 ms (70.64%) | Memory: 19 MB (47.78%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,22 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_complete(k):
+            sumi=0
+            for i in piles:
+                sumi+=math.ceil(i/k)
+            if sumi<=h:
+                return True
+            return False                
+            
+        low=1
+        high=max(piles)
+        ans=high
+        while low<=high:
+            mid=low+(high-low)//2
+            if can_complete(mid):
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
