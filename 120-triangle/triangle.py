cc6ddf73a671095900a66e230a28717757a72dfc
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2025-09-27 01:10:09 +0530

    Time: 3 ms (86.84%) | Memory: 18.9 MB (61.51%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Start from the second-to-last row and move upward
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                triangle[row][col] += min(triangle[row + 1][col],
+                                          triangle[row + 1][col + 1])
+        return triangle[0][0]
