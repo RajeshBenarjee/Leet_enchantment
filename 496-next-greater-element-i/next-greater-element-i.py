8127c1939a7d00a37769f0b4b2eb1b7ba39af030
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2026-03-27 04:51:26 +0530

    Time: 66 ms (6.47%) | Memory: 19.4 MB (82.97%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,10 +1,25 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        d1 = {}
-        st = []
-        for i in range(len(nums2) - 1, -1, -1):
-            while st and st[-1] < nums2[i]:
-                st.pop()
-            d1[nums2[i]] = st[-1] if st else -1
-            st.append(nums2[i])
-        return [d1[i] for i in nums1]
+    def nextGreaterElement(self, nums1, nums2):
+        n = len(nums2)
+        nge = [-1] * n
+        mono = []
+
+        # Build NGE array
+        for i in range(n - 1, -1, -1):
+            while mono and mono[-1] <= nums2[i]:
+                mono.pop()
+
+            if mono:
+                nge[i] = mono[-1]
+
+            mono.append(nums2[i])
+
+        # Find results for nums1
+        res = []
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    res.append(nge[j])
+                    break
+
+        return res
