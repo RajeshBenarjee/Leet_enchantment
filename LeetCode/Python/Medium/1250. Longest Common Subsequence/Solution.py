fc03543240aae09b8f9ded209f9d1ad7804474a7
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2026-07-30 03:01:00 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/1250. Longest Common Subsequence/Solution.py
+++ LeetCode/Python/Medium/1250. Longest Common Subsequence/Solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n1,n2=len(text1),len(text2)
-        dp=[[-1]*(n2+1) for _ in range(n1+1)]
-        def rec(i1,i2):
-            if i1<0 or i2<0:
-                return 0
-            if dp[i1][i2]!=-1:
-                return dp[i1][i2]
-            if text1[i1]==text2[i2]:
-                dp[i1][i2]= 1+rec(i1-1,i2-1)
-                return dp[i1][i2]
-            else:
-                dp[i1][i2]=0+max(rec(i1-1,i2),rec(i1,i2-1))
-                return dp[i1][i2]
-        return rec(n1-1,n2-1)
+        n1, n2 = len(text1), len(text2)
+
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n1][n2]
+        # def rec(i1,i2):
+        #     if i1<0 or i2<0:
+        #         return 0
+        #     if dp[i1][i2]!=-1:
+        #         return dp[i1][i2]
+        #     if text1[i1]==text2[i2]:
+        #         dp[i1][i2]= 1+rec(i1-1,i2-1)
+        #         return dp[i1][i2]
+        #     else:
+        #         dp[i1][i2]=0+max(rec(i1-1,i2),rec(i1,i2-1))
+        #         return dp[i1][i2]
+        # return rec(n1-1,n2-1)
