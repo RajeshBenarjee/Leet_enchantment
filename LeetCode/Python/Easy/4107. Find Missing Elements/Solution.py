5d822f66a9154c1e13b6102e9a0b6fe5c3513b65
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2026-08-04 14:53:32 +0530

    Add Easy Python Solution from LeetCode

---
+++ LeetCode/Python/Easy/4107. Find Missing Elements/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        start,end=min(nums),max(nums)
+        org=set(i for i in range(start,end+1))
+        have=set(nums)
+        return sorted(list(org-have))
