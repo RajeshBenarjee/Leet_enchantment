c5edffe6d7c1e70928231b31902ee4c2675679db
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2026-09-23 10:19:30 +0530

    Add Medium Python Solution for 739. Daily Temperatures from LeetCode

--- LeetCode/Python/Medium/739. Daily Temperatures/README.md
+++ LeetCode/Python/Medium/739. Daily Temperatures/README.md
@@ -8,8 +8,8 @@
 Array, Stack, Monotonic Stack
 
 ### 🚀 Performance
-- **Runtime:** 104 ms
-- **Memory:** 28 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Python/Medium/739. Daily Temperatures/Solution.py
+++ LeetCode/Python/Medium/739. Daily Temperatures/Solution.py
@@ -3,10 +3,12 @@ class Solution:
         n=len(arr)
         res=[0]*n
         st=[]
-        for i in range(n-1,-1,-1):
+        for i in range(len(arr)-1,-1,-1):
             while st and arr[st[-1]]<=arr[i]:
                 st.pop()
             if st:
                 res[i]=st[-1]-i
             st.append(i)
         return res
+
+            
